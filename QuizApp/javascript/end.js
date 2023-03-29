fa989f16521269693b17fe0f8cadac99395a8215
Buteya <buteyamasira@gmail.com>
2023-03-29 19:28:02 +0300

    added function to capitalize the first letter of a word or sentence

--- QuizApp/javascript/end.js
+++ QuizApp/javascript/end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -12,4 +15,14 @@ username.addEventListener('keyup',()=>{
 saveHighScore = ()=>{
     console.log("clicked the save button");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random()*100),
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a,b)=> b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores',JSON.stringify(highScores));
+    window.location.assign("/");
 }
