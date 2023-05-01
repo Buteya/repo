978e664f123333760de7498575f842536c04dd46
Buteya <buteyamasira@gmail.com>
2023-05-01 12:16:24 +0300

    added and reomoved class hiden from game to loader

--- QuizApp/javascript/game.js
+++ QuizApp/javascript/game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 
 let currentQuestion = {};
@@ -12,26 +14,27 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [];
-fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple").then( res =>{
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple").then(res => {
     return res.json();
-}).then(loadedQuestions =>{
+}).then(loadedQuestions => {
     console.log(loadedQuestions.results);
-    questions = loadedQuestions.results.map(loadedQuestions =>{
+    questions = loadedQuestions.results.map(loadedQuestions => {
         const formattedQuestion = {
             question: loadedQuestions.question,
         };
         const answerChoices = [...loadedQuestions.incorrect_answers];
         formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-        answerChoices.splice(formattedQuestion.answer - 1,0,loadedQuestions.correct_answer);
+        answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestions.correct_answer);
 
-        answerChoices.forEach((choice, index) =>{
+        answerChoices.forEach((choice, index) => {
             formattedQuestion["choice" + (index + 1)] = choice;
         });
 
         return formattedQuestion;
-    })
+    });
+
     startGame();
-}).catch(err=>{
+}).catch(err => {
     console.error(err);
 });
 
@@ -39,61 +42,63 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
-startGame = ()=>{
+startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
-getNewQuestion = ()=>{
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore',score);
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
         //go to the end page
         return window.location.assign('C:/Users/butey/OneDrive/Desktop/IST/QuizApp/html/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     // update the progress bar
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random()*availableQuestions.length);
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
     choices.forEach(choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice'+ number];
+        choice.innerText = currentQuestion['choice' + number];
     });
 
-    availableQuestions.splice(questionIndex,1);
+    availableQuestions.splice(questionIndex, 1);
 
-    acceptingAnswers=true;
+    acceptingAnswers = true;
 };
 
-choices.forEach(choice =>{
-    choice.addEventListener('click', e =>{
-        if(!acceptingAnswers)return;
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
 
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-        if(classToApply === 'correct'){
+        if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
 
         selectedChoice.parentElement.classList.add(classToApply);
-        setTimeout(()=>{
+        setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
-        },1000);
-        
+        }, 1000);
+
     });
 });
 
-incrementScore = num =>{
+incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 };
