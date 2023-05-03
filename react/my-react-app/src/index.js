5f0c99e3812723288437a361949a7826fb6d5958
Buteya <buteyamasira@gmail.com>
2023-05-03 21:37:38 +0300

    added a table

--- react/my-react-app/src/index.js
+++ react/my-react-app/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
+const myFirstElement = <div>
+    <h1>Hello React!</h1>
+    <table border={1}>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>John</td>
+        </tr>
+        <tr>
+            <td>Elsa</td>
+        </tr>
+    </table>
+    </div>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
