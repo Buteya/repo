7682a0ba4e98b4e8495f8ecf21fd7adc0ded86c5
Buteya <buteyamasira@gmail.com>
2023-05-24 17:13:55 +0300

    modified component components/Car.js to use state

--- react/my-react-app/src/components/Car.js
+++ react/my-react-app/src/components/Car.js
@@ -1,7 +1,28 @@
 import styles from '../css/my-style.module.css';
+import {useState} from 'react';
 
 const NewCar=()=>{
-    return <h2 className={styles.bigblue}>I am also a car!</h2>
+    const [car, setCar] = useState({
+        brand:"Ford",
+        model:"Mustang",
+        year:"1964",
+        color:"red",
+    });
+
+    const updateColor= ()=>{
+        setCar(previousCar =>{
+            return {...previousCar, color:"blue"}
+        })
+    }
+    return(
+        <>
+            <h1>My {car.brand}</h1>
+            <p>
+                It is a {car.color} {car.model} from {car.year}.
+            </p>
+            <button type="button" onClick={updateColor}Blue></button>
+        </>
+    )
 }
 
 export default NewCar;
