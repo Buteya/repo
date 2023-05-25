4a4f1ccfc41fc2f16d1b683b51c01c3ed436931b
Buteya <buteyamasira@gmail.com>
2023-05-25 17:14:25 +0300

    added validation

--- carwebsite/javascript/loginsignup.js
+++ carwebsite/javascript/loginsignup.js
@@ -2,11 +2,24 @@ const loginBtn = document.getElementById('login-btn');
 const signUpBtn = document.getElementById('signup-btn');
 const usernameDiv = document.getElementById('username-div');
 const confirmPasswordDiv = document.getElementById('confirm-password-div');
+const username = document.forms["login-form"]["username"].value;
+const email = document.forms["login-form"]["email"].value;
+const password = document.forms["login-form"]["password"].value;
+const confirmPassword = document.forms["login-form"]["confirm-password"].value;
+
+let signingup = false;
+let logingin = false;
 
 // setup form validation
 const login = ()=>{
     usernameDiv.style.display = 'none';
     confirmPasswordDiv.style.display = 'none';
+    loginBtn.classList.add("btn-primary");
+    loginBtn.classList.remove("btn-outline-primary");
+    signUpBtn.classList.remove("btn-primary");
+    signUpBtn.classList.add("btn-outline-primary"); 
+    logingin += !logingin;
+    setTimeout(()=>validateForm(),500);
 }
 
 const signUp = ()=>{
@@ -16,4 +29,29 @@ const signUp = ()=>{
     loginBtn.classList.add("btn-outline-primary");
     signUpBtn.classList.remove("btn-outline-primary");
     signUpBtn.classList.add("btn-primary");
+    signingup += !signingup;
+   setTimeout(()=>validateForm(),500);
+}
+
+const validateForm = ()=>{
+    if(logingin){
+        if(email == "" && password == ""){
+            alert("Email and Password is required");
+        }else if(email == ""){
+            alert("Email is required");
+        }else if(password == ""){
+            alert("Password is required");
+        }
+    }
+    if(signingup){
+        if(username == "" && email == "" && password == ""){
+        alert("Username, Email and Password must be filled out");
+    }else if(email == ""){
+        alert("Email must be filled out");
+    }else if(password == ""){
+        alert("Password must be filled out");
+    }else if(confirmPassword == ""){
+        alert("Confirm Password must be filled out");
+    }
+    }  
 }
