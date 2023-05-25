49c50368ee5080f30f9764993a17a723c178fb79
Buteya <buteyamasira@gmail.com>
2023-05-25 18:24:40 +0300

    added state calculation multiply count

--- react/my-react-app/src/components/Timer.js
+++ react/my-react-app/src/components/Timer.js
@@ -2,14 +2,26 @@ import {useState,useEffect} from 'react';
 
 const Timer = ()=>{
     const [count,setCount] = useState(0);
+    const [calculation, setCalculation] = useState(0);
 
     useEffect(()=>{
-        setTimeout(()=>{
-            setCount(count => count + 1);
-        },1000);
-    },[]);
+        setCalculation(()=>count * 2);
+        // let timer = setTimeout(()=>{
+        //     setCount(count => count + 1);
+        // },1000);
+        // return ()=> clearTimeout(timer);
+    },[count]);
 
-    return <h1>I`ve render {count} times!</h1>
+    
+
+    return(
+        <>
+            {/* <h1>I`ve rendered {count} times!</h1> */}
+            <p>Count: {count}</p>
+            <button onClick={()=>setCount((c)=>c+1)}>+</button>
+            <p>Calculation: {calculation}</p>
+        </>
+    )
 };
 
 export default Timer;
