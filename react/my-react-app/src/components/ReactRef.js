0fced739eb51b34e7bd1f26c96860e18192caac7
Buteya <buteyamasira@gmail.com>
2023-05-29 19:02:11 +0300

    edited ReactRef to show how useRef can be used to track state changes since it can persist state through renders

--- react/my-react-app/src/components/ReactRef.js
+++ react/my-react-app/src/components/ReactRef.js
@@ -2,11 +2,11 @@ import {useEffect,useState,useRef} from 'react';
 
 const ReactRef = ()=>{
     const [inputValue, setInputValue] = useState("");
-    const count = useRef(0);
+    const previousInputValue = useRef("");
   
     useEffect(() => {
-      count.current = count.current + 1;
-    });
+      previousInputValue.current = inputValue;
+    },[inputValue]);
   
     return (
       <>
@@ -15,7 +15,8 @@ const ReactRef = ()=>{
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <h1>Render Count: {count.current}</h1>
+        <h1>Current Value: {inputValue}</h1>
+        <h1>Previous Value: {previousInputValue.current}</h1>
       </>
     );
   }
