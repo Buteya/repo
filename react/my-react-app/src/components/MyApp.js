c70d205e3164fdc1ced637467a6aeb898db20975
Buteya <buteyamasira@gmail.com>
2023-06-01 17:42:14 +0300

    added useMemo properly

--- react/my-react-app/src/components/MyApp.js
+++ react/my-react-app/src/components/MyApp.js
@@ -1,18 +1,20 @@
-import {useState,useCallback} from 'react';
+import {useState,useMemo} from 'react';
 import MyTodos from './MyTodos';
 
 const MyApp = () => {
     const [count, setCount] = useState(0);
     const [todos, setTodos] = useState([]);
+    const calculation = useMemo(()=>expensiveCalculation(count),[count]);
   
     const increment = () => {
       setCount((c) => c + 1);
     };
-    const addTodo = useCallback(() => {
+    const addTodo = () => {
       setTodos((t) => [...t, "New Todo"]);
-    },[todos]);
+    };
   
     return (
+      <>
       <>
         <MyTodos todos={todos} addTodo={addTodo} />
         <hr />
@@ -21,7 +23,24 @@ const MyApp = () => {
           <button onClick={increment}>+</button>
         </div>
       </>
+      <hr />
+      <>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </>
+      </>
     );
   };
+
+  const expensiveCalculation = (num)=>{
+    console.log('Calculating ...');
+    for(let i = 0; i < 10000000000;i++){
+      num += i;
+    }
+
+    return num;
+  }
 
 export default MyApp;
