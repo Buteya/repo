aea7c7b9b2a6d775c34d721320fcea23a74d2984
Buteya <buteyamasira@gmail.com>
2023-06-01 14:58:40 +0300

    added comments

--- python/HelloWorld.py
+++ python/HelloWorld.py
@@ -1,3 +1,15 @@
-print("Hello World") 
+# print("Hello World") 
+print("Cheers!, Mate")
+
 if 5 > 2:
     print("Five is greater than two!")
+
+#This is a comment
+
+#This is a multiline
+#comment.
+
+"""
+Python ignores an usigned string literals
+You can use this to write multiline comments.
+"""
