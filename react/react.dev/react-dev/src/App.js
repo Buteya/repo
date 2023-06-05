2751d47a2d5ba907342563964b687f31393c29ab
Buteya <buteyamasira@gmail.com>
2023-06-05 16:26:28 +0300

    added a new react app for react.dev called react-dev

---
+++ react/react.dev/react-dev/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyButton from './components/MyButton.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My App</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
