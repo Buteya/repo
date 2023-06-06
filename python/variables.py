2685cebb8f05cc6a977d657cbc427f841eea524a
Buteya <buteyamasira@gmail.com>
2023-06-06 16:10:24 +0300

    showed how to unpack collections, add a single value to multiple variables

--- python/variables.py
+++ python/variables.py
@@ -41,3 +41,22 @@ MyVariableName = "John"
 
 # Snake case
 my_variable_name = "John"
+
+# Assign multiple values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One value to multiple variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Unpack a collection
+fruits = ["Orange","Banana","Cherry"]
+x, y, z = fruits 
+print(x)
+print(y)
+print(z)
