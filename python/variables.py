bf00a74a3a08776c6be469177325b4096bd5bbb0
Buteya <buteyamasira@gmail.com>
2023-06-07 16:22:47 +0300

    showed how to output variables

--- python/variables.py
+++ python/variables.py
@@ -60,3 +60,25 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z)
+
+# Output variables
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y,z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5 
+y = "John"
+print(x,y)
