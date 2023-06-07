539e4f98fa67af8dd8d9c2244b1430e6d683e7c6
Buteya <buteyamasira@gmail.com>
2023-06-07 15:50:28 +0300

    added styling through className and App.css

--- react/react.dev/react-dev/src/App.js
+++ react/react.dev/react-dev/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import MyButton from './components/MyButton.js';
 import AboutPage from './components/AboutPage.js';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to My App</h1>
       <MyButton />
-      <AboutPage />
+      <AboutPage/>
     </div>
   );
 }
--- react/react.dev/react-dev/src/components/AboutPage.js
+++ react/react.dev/react-dev/src/components/AboutPage.js
@@ -1,9 +1,9 @@
 const AboutPage = ()=>{
     return(
-        <>
+        <div className="avatar">
             <h1>About</h1>
             <p>Hello there, <br />How do you do?</p>
-        </>
+        </div>
     )
 }
 
