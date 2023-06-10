8da5da5d4ada74f441984a15ac9f83505063a8e4
Buteya <buteyamasira@gmail.com>
2023-06-10 14:08:21 +0300

    added component profile

--- react/react.dev/react-dev/src/App.js
+++ react/react.dev/react-dev/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MyButton from './components/MyButton.js';
 import AboutPage from './components/AboutPage.js';
+import Profile from './components/Profile.js';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <h1>Welcome to My App</h1>
       <MyButton />
       <AboutPage/>
+      <Profile />
     </div>
   );
 }
