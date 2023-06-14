26ca604347b4c37cf9a43bbdb9227d5b464117de
Buteya <buteyamasira@gmail.com>
2023-06-14 11:28:03 +0300

    used state in component MyButton

--- react/react.dev/react-dev/src/components/MyButton.js
+++ react/react.dev/react-dev/src/components/MyButton.js
@@ -1,8 +1,10 @@
+import {useState} from 'react';
 const MyButton = ()=>{
+    const [count,setCount] = useState(0);
     const handleClick = ()=>{
-        alert('You clicked me!');
+        setCount(count + 1)
     }
-    return (<button onClick={handleClick}>Click Me</button>);
+    return (<button onClick={handleClick}>Click {count} times</button>);
 }
 
 export default MyButton;
