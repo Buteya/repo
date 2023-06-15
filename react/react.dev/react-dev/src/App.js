b8ec3594648c6794790c2f5e9ce36377b683c2ef
Buteya <buteyamasira@gmail.com>
2023-06-15 12:11:42 +0300

    lifted MyButton state up

--- react/react.dev/react-dev/src/App.js
+++ react/react.dev/react-dev/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from 'react';
 import MyButton from './components/MyButton.js';
 import AboutPage from './components/AboutPage.js';
 import Profile from './components/Profile.js';
@@ -7,10 +8,16 @@ import ShoppingList from './components/ShoppingList.js';
 
 
 function App() {
+  const [count,setCount] = useState(0);;
+
+  function handleClick(){
+    setCount(count + 1)
+  }
   return (
     <div className="App">
       <h1>Welcome to My App</h1>
-      <MyButton />
+      <MyButton count={count} onClick={handleClick}/>
+      <MyButton count={count} onClick={handleClick}/>
       <AboutPage/>
       <Profile />
       <Content />
--- react/react.dev/react-dev/src/components/MyButton.js
+++ react/react.dev/react-dev/src/components/MyButton.js
@@ -1,10 +1,6 @@
-import {useState} from 'react';
-const MyButton = ()=>{
-    const [count,setCount] = useState(0);
-    const handleClick = ()=>{
-        setCount(count + 1)
-    }
-    return (<button onClick={handleClick}>Click {count} times</button>);
+const MyButton = ({count,onClick})=>{
+ 
+    return (<button onClick={onClick}>Click {count} times</button>);
 }
 
 export default MyButton;
