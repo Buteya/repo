4a48cba448a1e499c2bff71c15d9a5d810fc2482
Buteya <buteyamasira@gmail.com>
2023-06-26 19:08:12 +0300

    connected to mongodb

--- node/firstapi/index.js
+++ node/firstapi/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
-const app = express();
+require('dotenv').config();
+require('./helpers/init_mongoDB');
+
 
 const studentRoute = require('./routes/students.routes');
+
+const app = express();
+
+app.use(express.json());
+
+//handling 404 error
+app.use((req,res,next)=>{
+    const err =  new Error("Not Found");
+    err.status = 404;
+    next(err);
+})
+
+//Error handler
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500);
+    res.send({
+        error:{
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
+
 app.use('/students',studentRoute);
 
 app.listen(process.env.port || 4000, function(){
--- node/firstapi/routes/students.routes.js
+++ node/firstapi/routes/students.routes.js
@@ -7,8 +7,15 @@ routes.get('/',(req,res,next)=>{
 });
 
 //add student to the db
-routes.post('/',(req,res,next)=>{
-    res.send({type:'Post request'});
+routes.post('/',async(req,res,next)=>{
+    try{
+        const student = new Student(re.body);
+        const result = await student.save();
+        res.send(result)
+    } catch(error){
+        console.log(error.message);
+    }
+    
 });
 
 //update students in the DB
