78b7bc2b5ce5352c9feefd15f1f6e8046d976632
Buteya <buteyamasira@gmail.com>
2023-07-07 12:25:03 +0300

    added auth route

---
+++ node/firstapi/routes/auth.route.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const Users = require('../models/user')
+const routes = express.Router();
+const httpErrors= require('http-errors');
+
+routes.post('/register',async(req,res,next)=>{
+    try {
+        const{email,password}=req.body;
+        if(!email||!password)throw httpErrors.BadRequest();
+
+        const Exists =await Users.findOne({email:email})
+
+    if(Exists)throw httpErrors.Conflict(`${email}is already been used`)
+
+    const user = new Users({email,password})
+    const saveUser=await user.save()
+    res.send(saveUser)
+    } catch (error) {
+        console.log(error.message)
+        next(error)
+    }
+   
+});
+routes.get('/login',async(req,res,next)=>{
+    // req.send({type:"login is successful"})
+   try{
+    const result = await Users.find();
+    res.send(result);
+   }
+   catch(error){
+    console.log(error.message)
+    next(error)
+   }
+});
+routes.delete('/userDelete',(req,res,next)=>{
+    res.send({type:"user has been deleted"})
+});
+routes.delete('/logout',(req,res,next)=>{
+    res.send({type:"logout route"})
+});
+routes.post('/refresh-toke',async(req,res)=>{
+    res.send('refresh-token route')
+})
+module.exports=routes;
--- node/firstapi/routes/students.routes.js
+++ node/firstapi/routes/students.routes.js
@@ -1,67 +1,11 @@
-const express =  require('express');
-const Student = require('../models/student');
+const express =require('express');
+const { getAllStudents, getStudentById, postStudent, replaceStudentById, deleteStudent, updateStudent } = require('../controllers/student.controller');
 const routes = express.Router();
-
-//get all students
-routes.get('/',async(req,res,next)=>{
-    try{
-        const result =await Student.find();
-        res.send(result);
-    }catch(error){
-        console.log(error.message);
-    }
-});
-
-//add student to the db
-routes.post('/',async(req,res,next)=>{
-    try{
-        const student = new Student(req.body);
-        const result = await student.save();
-        res.send(result)
-    } catch(error){
-        console.log(error.message);
-    }
-    
-});
-
-//get student by id
-routes.get('/:id',async(req,res,next)=>{
-    const id = req.params.id;
-    try{
-        const student = await Student.findById(id);
-        res.send(student);
-    }catch(error){
-        console.log(error.message);
-    }
-})
-
-//update students in the DB
-routes.put('/:id',(req,res,next)=>{
-    res.send({type:'Update request'});
-});
-
-//update student
-routes.patch('/:id',async(req,res,next)=>{
-    try{
-        const id = req.params.id;
-        const update = req.body;
-        const options = {new:true};
-        const result = await Student.findByIdAndUpdate(id,update,options);
-        res.send(result);
-    }catch(error){
-        console.log(error.message);
-    }
-})
-
-//delete a student from the DB
-routes.delete('/:id',async(req,res,next)=>{
-    const id = req.params.id;
-    try{
-        const student = await Student.findByIdAndRemove(id);
-        res.send(student);
-    }catch(error){
-        console.log(error.message);
-    }
-});
-
-module.exports = routes;
+routes.get('/',getAllStudents);
+routes.get('/:id',getStudentById);
+routes.post('/',postStudent);
+routes.put('/:id',replaceStudentById);
+routes.delete('/:id',deleteStudent);
+routes.patch('/:id',updateStudent);
+routes.get('/:firstName,')
+module.exports=routes;
