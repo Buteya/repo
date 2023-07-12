f01ec6b94ade5d613e2d330a93155739b6faaba6
Buteya <buteyamasira@gmail.com>
2023-07-12 16:27:53 +0300

    looping through list

--- python/list_1.py
+++ python/list_1.py
@@ -120,3 +120,24 @@ del thislist
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist)
+
+#looping through list
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+  
+#loop through the index numbers
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+#using a while loop
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+#looping using list compression
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
