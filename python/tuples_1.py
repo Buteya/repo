d531af86636e9972cc1813ec35e1fc6c56f79999
Buteya <buteyamasira@gmail.com>
2023-07-24 17:56:02 +0300

    using range

--- python/tuples_1.py
+++ python/tuples_1.py
@@ -29,3 +29,34 @@ print(type(mytuple))
 #using the constructor
 thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
 print(thistuple)
+
+#access tuples
+#using the index
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+#starting from the end
+#the last item
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+#range of indexes
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+#returnings items from the beginning
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+#returning items from a specific index
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#starting range from the back
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+#check if item exists
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
