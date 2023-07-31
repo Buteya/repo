1c10dd13458319ec6cf432c5a16f4091335fe110
Buteya <buteyamasira@gmail.com>
2023-07-31 20:35:46 +0300

    add tuples

--- c#/CsharpProjects/TestProject/Program.cs
+++ c#/CsharpProjects/TestProject/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Steve!");
-Random dice = new Random();
+Random dice = new();
 int roll = dice.Next(1, 7);
 Console.WriteLine(roll);
 
-Random dice1 = new Random();
+Random dice1 = new();
 int roll1 = dice1.Next();
 int roll2 = dice1.Next(101);
 int roll3 = dice1.Next(50, 101);
--- python/tuples_1.py
+++ python/tuples_1.py
@@ -144,3 +144,15 @@ while i < len(thistuple):
   print(thistuple[i])
   i = i + 1
 
+#join tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#multiply tuples
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
