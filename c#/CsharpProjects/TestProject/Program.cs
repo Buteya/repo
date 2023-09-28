93d32494a92317089ef698e3aff3864b0756589d
Buteya <buteyamasira@gmail.com>
2023-09-28 18:33:14 +0300

    Random program

--- c#/CsharpProjects/TestProject/Program.cs
+++ c#/CsharpProjects/TestProject/Program.cs
@@ -1,20 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Steve!");
 Random dice = new();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
 
-Random dice1 = new();
-int roll1 = dice1.Next();
-int roll2 = dice1.Next(101);
-int roll3 = dice1.Next(50, 101);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+    Console.WriteLine("You rolled doubles! +2 bonus added to total!");
+    total +=2;
+}
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
-largerValue = Math.Max(firstValue, secondValue);
-Console.WriteLine(largerValue);
+if((roll1 == roll2) && (roll2 == roll3)){
+    Console.WriteLine("You rolled triples! +6 bonus added to total!");
+    total +=6;
+}
+
+if(total >= 15){
+    Console.WriteLine("You win!");
+}
+
+if(total < 15 ){
+    Console.WriteLine("Sorry, You lose!");
+}
+
+string message = "The quick brown fox jumps over the lazy dog";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if(message.Contains("fox")){
+    Console.WriteLine("What does the fox say?");
+}
